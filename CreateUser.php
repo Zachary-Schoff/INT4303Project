6b4b8fa96f27406be699eb9ad6e172136ad2e00b
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-03-23 17:27:16 -0400

    Image Functionality Work

--- CreateUser.php
+++ CreateUser.php
@@ -1,6 +1,49 @@
 <?php
 include 'Connection.php';
 
+$target_dir = "image/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+
 $fname = $_REQUEST['fname'];
 $lname = $_REQUEST['lname'];
 $email = $_REQUEST['email'];
@@ -8,19 +51,6 @@ $dob = $_REQUEST['dob'];
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 $bio = $_REQUEST['bio'];
-
-$imagedir = "images/";
-$imagefile = $imagedir.basename($_FILES["pfp"]["name"]);
-$imageFileType = strtolower(pathinfo($imagefile, PATHINFO_EXTENSION));
-
-if(isset($_POST["submit"])){
-	$check = getimagesize($_FILES["pfp"]["tmp_name"]);
-	if($check !== false) {
-		echo "Success!";
-	} else {
-		echo "File selected is not an image: ".$check["mime"].".";
-	}
-}
 
 $sql = " CALL createuser('$fname', '$lname', '$email', '$dob', '$username', '$password', '$bio', '$email.$imageFileType');";
 
@@ -55,7 +85,7 @@ $conn->close();
 		<label>Username</label><br>
 		<input type="text" id="username" name="username"><br>
 		<label>Profile picture</label><br>
-		<input type="file" id="pfp" name="pfp"><br>
+		<input type="file" id="fileToUpload" name="fileToUpload"><br>
 		<label>Password</label><br>
 		<input type="text" id="password" name="password"><br>
 		<label>First Name</label><br>
