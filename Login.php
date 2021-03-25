0e980485d0acbad512030d7c461422102d23cbbf
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-03-25 17:40:18 -0400

    Session that pulls from an array of the SQL Query Results to populate UID

--- Login.php
+++ Login.php
@@ -4,50 +4,15 @@ include 'Connection.php';
 $email = $_REQUEST["Email"];
 $password = $_REQUEST["password"];
 
-$sql = " SELECT * FROM user WHERE email = '$email' AND password = '$password';";
+$sql = " SELECT * FROM user WHERE email = '$email' AND password = '$password' LIMIT 1;";
 
-$uidsql = " SELECT userid FROM user WHERE email = '$email' AND password = '$password';";
-
-$uid = $conn->query($uidsql);
-
-$result = $conn->query($sql);
-
-if($result->num_rows == 0){
-	echo "Login failed";
-} else {
-	echo "Logged in";
-	
-	session_start();
-	$_SESSION["userid"] = $uid;
+if ($result = $conn->query($sql)){
+	while($row = $result->fetch_row()){
+		print_r($row);
+	}
 }
+else{
+	// If it is empty
+}
+
 ?>
-
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-	<link href = "Style.css" rel = "stylesheet">
-</head>
-
-<body>
-	<h1>LTU Resource</h1>
-	<div class="topnav">
-		<a class="dash" href="Dashboard.html">Dashboard</a>
-		<a class="dash" href="News.html">News</a>
-		<a class="dash" href="Search.html">Search</a>
-		<a class="dash" href="Profile.html">Profile</a>
-		<a class="dash" href="About.html">About</a>
-		<a class="dash" href="Login.html">Login</a>
-	</div>
-	<form method="post" action="Login.php">
-		<label for="username">Email</label><br>
-		<input type="text" id="Email" name="Email"><br>
-		<label for="password">Password</label><br>
-		<input type="text" id="password" name="password">
-		<input type="submit" value="Login">
-	</form>
-	
-	<a href="CreateUser.html">Create Account</a>
-</body>
-</html>
