63f01d30dfdb653b6fdcb25494ce0d01d31cbd93
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-03-31 15:41:08 -0400

    First pass Profile page
    Real simple, built into Login HTML so that Nik can implement it into the rest of the site later.

--- Login.php
+++ Login.php
@@ -9,10 +9,29 @@ $sql = " SELECT * FROM user WHERE email = '$email' AND password = '$password' LI
 if ($result = $conn->query($sql)){
 	while($row = $result->fetch_row()){
 		print_r($row);
+		$uid = $row[0];
+		$nickname = $row[1];
+		$picture = $row[4];
+		$bio = $row[5];
+		$fname = $row[6];
+		$lname = $row[7];
+		$dob = $row[8];
 	}
 }
 else{
-	// If it is empty
+	echo "That account does not exist. Please make sure your email and password are correct.";
 }
-
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link href = "Style.css" rel = "stylesheet">
+</head>
+
+<body>
+	<h1>My Profile</h1>
+	<img src="image/$picture" alt = "Profile Picture">
+</body>
+</html>
