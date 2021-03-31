352af489912d307e92eaa82a85ce88147730b2fc
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-03-31 15:52:20 -0400

    Revamp of the Profile page
    Changed it to try and actually work

--- Login.php
+++ Login.php
@@ -11,27 +11,22 @@ if ($result = $conn->query($sql)){
 		print_r($row);
 		$uid = $row[0];
 		$nickname = $row[1];
-		$picture = $row[4];
+		$picture = "images/".$row[4];
 		$bio = $row[5];
 		$fname = $row[6];
 		$lname = $row[7];
 		$dob = $row[8];
+		
+		echo(
+			"<h1>My Profile</h1></br>
+			<img src = ".$picture."/></br>
+			<p>Nickname: ".$nickname."</p></br>
+			<p>First Name: ".$fname."</p></br>
+			<p>Last Name: ".$lname."</p></br>"
+		);
 	}
 }
 else{
 	echo "That account does not exist. Please make sure your email and password are correct.";
 }
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-	<link href = "Style.css" rel = "stylesheet">
-</head>
-
-<body>
-	<h1>My Profile</h1>
-	<img src="image/$picture" alt = "Profile Picture">
-</body>
-</html>
