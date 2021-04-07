c26a0d8aeb8cf4bb61d5b359f3d9066d9744cc39
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-04-07 16:01:52 -0400

    Major Login PHP Update
    Added a metric ton of logic to display the other users you are following when you login. Also to maintain normal functionality if you are not following someone. Aim to put this on the follow page as well, but one step at a time

--- Login.php
+++ Login.php
@@ -26,24 +26,71 @@ if ($result = $conn->query($sql)){
 		$_SESSION["lname"] = $lname;
 		$_SESSION["dob"] = $dob;
 		
-		echo(
-			"<html>
-				<head>
-					<meta charset='utf-8'>
-					<title>Untitled Document</title>
-					<link href = 'Style.css' rel = 'stylesheet'>
-				</head>
-				<body>
-					<h1>My Profile</h1><form method='post' action='Follow.php'><input type='submit' value='Follow'></form></br>
-					<img src = '".$picture."' height='200' width='200'/></br>
-					<p>Nickname: ".$nickname."</p></br>
-					<p>First Name: ".$fname."</p></br>
-					<p>Last Name: ".$lname."</p></br>
-					<p>Bio: ".$bio."</p></br>
-					<p>Birthday: ".$dob."</p></br>
-				</body>
-			</html>"
-		);
+		$followsql = "SELECT user.userid, following.followid, following.followerid FROM user JOIN following ON user.userid=following.followerid;";
+		
+		if ($fresult = $conn->query($followsql)){
+			while($row = $result->fetch_row()){
+				$pullfollow = "SELECT * FROM user WHERE userid = $row[0];";
+				if ($fresult = $conn->query($pullfollow)){
+					while($row = $result->fetch_row()){
+						
+						$fuid = $row[0];
+						$fnickname = $row[1];
+						$fpicture = "image/".$row[4];
+						$fbio = $row[5];
+						$ffname = $row[6];
+						$flname = $row[7];
+						$fdob = $row[8];
+						
+						echo (
+							"<html>
+								<head>
+									<meta charset='utf-8'>
+									<title>Untitled Document</title>
+									<link href = 'Style.css' rel = 'stylesheet'>
+								</head>
+								<body>
+									<h1>My Profile</h1><form><input type='submit' value='Followed'></form></br>
+									<img src = '".$picture."' height='200' width='200'/></br>
+									<p>Nickname: ".$nickname."</p></br>
+									<p>First Name: ".$fname."</p></br>
+									<p>Last Name: ".$lname."</p></br>
+									<p>Bio: ".$bio."</p></br>
+									<p>Birthday: ".$dob."</p></br>
+									<h1>Following</h1></br>
+									<img src = '".$fpicture."' height='200' width='200'/></br>
+									<p>Nickname: ".$fnickname."</p></br>
+									<p>First Name: ".$ffname."</p></br>
+									<p>Last Name: ".$flname."</p></br>
+									<p>Bio: ".$fbio."</p></br>
+									<p>Birthday: ".$fdob."</p></br>
+								</body>
+							</html>"
+						);
+					}
+				}
+			}
+		}
+		else{
+			echo (
+				"<html>
+					<head>
+						<meta charset='utf-8'>
+						<title>Untitled Document</title>
+						<link href = 'Style.css' rel = 'stylesheet'>
+					</head>
+					<body>
+						<h1>My Profile</h1><form><input type='submit' value='Followed'></form></br>
+						<img src = '".$picture."' height='200' width='200'/></br>
+						<p>Nickname: ".$nickname."</p></br>
+						<p>First Name: ".$fname."</p></br>
+						<p>Last Name: ".$lname."</p></br>
+						<p>Bio: ".$bio."</p></br>
+						<p>Birthday: ".$dob."</p></br>
+					</body>
+				</html>"
+			);
+		}
 	}
 }
 else{
