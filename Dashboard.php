319d0f86b3e5d575939d0be3a6e6640b111cb337
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-05-05 15:46:34 -0400

    Merged Dashboard code from Nik and I

--- Dashboard.php
+++ Dashboard.php
@@ -14,6 +14,7 @@ if (isset($_SESSION["user"]) && !empty($_SESSION["user"])){
 	<title>Untitled Document</title>
 		<link href = 'Dashboard.php'>
 		<link href = 'Dashboard.php' rel = 'connection'>
+		<link href='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet' id='bootstrap-css'>
 		<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6' crossorigin='anonymous'>
 		<link href = 'Style.css' rel = 'stylesheet'>
 	</head>
@@ -26,15 +27,15 @@ if (isset($_SESSION["user"]) && !empty($_SESSION["user"])){
 			<a class='dash' href='Profile.html'>Profile</a>
 			<a class='dash' href='About.php'>About</a>
 			<a class='dash' href='Logout.php'>Logout</a>
-			<a>
-				<form class='dash' action = 'SearchAction.php'>
-					<input class='form-control me-2'  id = 'input' name = 'input' type='text' placeholder='Search' aria-label='Search' style = 'width: 100%; text-align: right'>
-					<input class='btn btn-outline-success' type='submit'>
+			<a style = 'width'>
+				<form class='dash' action = 'Search.php'>
+					<input class='form-control me-2'  id = 'input' name = 'input' type='text' placeholder='Search' aria-label='Search' style = 'width: 70%; float: left'>
+					<input class='btn btn-outline-success' type='submit' style='float: right'>
 				</form>
 			</a>
 		</div>
 		<h3 class='title'>Dashboard</h3>
-		<a href='NewPost.php' style = 'color: red'>Add Post</a>
+		<a href='NewPost.html' style = 'color: white'>Add Post</a>
 	</body>
 	</html>";
 	}
