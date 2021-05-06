ca19603b10e184ec8dba5d93000e04851a370cc7
Zachary-Schoff <79164945+Zachary-Schoff@users.noreply.github.com>
2021-05-06 12:14:18 -0400

    Final Submission
    For real this time. Had issues with Github due to something Nik had done to it.
    
    This is my submission, made a myriad of changes with an eye toward consistency and mending broken functionalities like Forgot Password

--- About.php
+++ About.php
@@ -8,20 +8,27 @@ if (isset($_SESSION["user"]) && !empty($_SESSION["user"])){
 	<html>
 	<head>
 	<meta charset='utf-8'>
-	<title>Untitled Document</title>
+	<title>LTU Interlink</title>
+		<link href='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet' id='bootstrap-css'>
+		<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6' crossorigin='anonymous'>
 		<link href = 'Style.css' rel = 'stylesheet'>
 	</head>
 
 	<body>
-		<h1>LTU Resource</h1>
+		<h1 class = 'header'>LTU Interlink</h1>
 		<div class='topnav'>
 			<a class='dash' href='Dashboard.php'>Dashboard</a>
-			<a class='dash' href='News.html'>News</a>
+			<a class='dash' href='CreateGroup.php'>Groups</a>
 			<a class='dash' href='Search.php'>Search</a>
 			<a class='dash' href='Profile.html'>Profile</a>
 			<a class='dash' href='About.php'>About</a>
-			<a class='dash' href='Message.php'>Message</a>
 			<a class='dash' href='Logout.php'>Logout</a>
+			<a style = 'width'>
+				<form class='dash' action = 'SearchAction.php'>
+					<input class='form-control me-2'  id = 'input' name = 'input' type='text' placeholder='Search' aria-label='Search' style = 'width: 70%; float: left'>
+					<input class='btn btn-outline-success' type='submit' style='float: right'>
+				</form>
+			</a>
 		</div>
 		<h3>This website was created in order to help the Lawrence Tech Community have as many resources as needed to succeed</h3>
 
@@ -33,23 +40,29 @@ else{
 	<html>
 	<head>
 	<meta charset='utf-8'>
-	<title>Untitled Document</title>
+	<title>LTU Interlink</title>
+		<link href='//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet' id='bootstrap-css'>
+		<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6' crossorigin='anonymous'>
 		<link href = 'Style.css' rel = 'stylesheet'>
 	</head>
 
 	<body>
-		<h1>LTU Resource</h1>
+		<h1 class='header'>LTU Interlink</h1>
 		<div class='topnav'>
 			<a class='dash' href='Dashboard.php'>Dashboard</a>
 			<a class='dash' href='News.php'>News</a>
 			<a class='dash' href='Search.php'>Search</a>
 			<a class='dash' href='Profile.html'>Profile</a>
 			<a class='dash' href='About.php'>About</a>
-			<a class='dash' href='Message.php'>Message</a>
 			<a class='dash' href='Login.php'>Login</a>
+			<a style = 'width'>
+				<form class='dash' action = 'SearchAction.php'>
+					<input class='form-control me-2'  id = 'input' name = 'input' type='text' placeholder='Search' aria-label='Search' style = 'width: 70%; float: left'>
+					<input class='btn btn-outline-success' type='submit' style='float: right'>
+				</form>
+			</a>
 		</div>
 		<h3>This website was created in order to help the Lawrence Tech Community have as many resources as needed to succeed</h3>
-
 	</body>
 	</html>";
 }
